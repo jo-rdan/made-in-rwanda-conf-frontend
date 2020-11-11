b2ca46a5c6d31dc9ca73cd54c10c025cc31b8138
jo-rdan <jordankayinamura@gmail.com>
2020-11-11 02:54:24 -0800

    consume apis add validations add redirections

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
+import "react-native-gesture-handler";
 import React from "react";
 import { StyleSheet, Text, View, Platform, StatusBar } from "react-native";
 import { Provider } from "react-native-paper";
 import RegistrationScreen from "./app/screens/RegistrationScreen";
 import { Provider as PaperProvider } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Redirect from "./app/screens/Redirect";
+
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
     <PaperProvider>
-      <View style={styles.container}>
-        <RegistrationScreen />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {/* <View style={styles.container}>
+            <RegistrationScreen />
+          </View> */}
+          <Stack.Screen
+            name="Register"
+            component={RegistrationScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Redirect"
+            component={Redirect}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
--- app/screens/RegistrationScreen.js
+++ app/screens/RegistrationScreen.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { Image, View, StyleSheet, Text } from "react-native";
-
 import {
   TextInput,
   Button,
-  Paragraph,
   Dialog,
   Portal,
   RadioButton,
 } from "react-native-paper";
+import axios from "axios";
+import { Provider as PaperProvider } from "react-native-paper";
 
-function RegistrationScreen(props) {
-  const [text, setText] = React.useState("");
+function RegistrationScreen({ navigation }) {
   const [userData, setUserData] = React.useState({
     fname: "",
     lname: "",
@@ -21,10 +20,12 @@ function RegistrationScreen(props) {
     password_confirmation: "",
     role: "",
   });
-  const [focus, setFocus] = React.useState(false);
   const [visible, setVisible] = React.useState(false);
   const [checked, setChecked] = React.useState("6+ ");
-
+  const [errorMessage, setErrorMessage] = React.useState({
+    message: "",
+    show: false,
+  });
   const showDialog = () => setVisible(true);
 
   const hideDialog = () => setVisible(false);
@@ -32,125 +33,180 @@ function RegistrationScreen(props) {
   const handlePhone = (phone) => {
     setUserData({ ...userData, phone });
   };
-  const handleSubmit = () => {
-    console.log(userData);
+  const handleSubmit = async () => {
+    try {
+      const {
+        fname,
+        lname,
+        phone,
+        email,
+        password,
+        password_confirmation,
+        role,
+      } = userData;
+      const results = await axios.post(
+        "http://192.168.1.186:8000/api/register",
+        {
+          fname,
+          lname,
+          phone: `+250${phone}`,
+          email,
+          password,
+          password_confirmation,
+          role,
+        }
+      );
+      if (results.status === 201) return navigation.navigate("Redirect");
+    } catch (error) {
+      setErrorMessage({ message: error.response.data.error, show: true });
+      setTimeout(() => {
+        setErrorMessage({ ...errorMessage, show: false });
+      }, 3000);
+    }
+  };
+
+  const errorMessageStyles = {
+    display: !errorMessage.show ? "none" : "flex",
+    color: "#FF0F00",
+    fontWeight: "bold",
+    fontSize: 13,
+    textAlign: "center",
   };
   return (
-    <View style={styles.background}>
-      <Image source={require("../assets/logo.png")} />
-      <View style={styles.registerForm}>
-        <Text style={styles.title}>Register</Text>
-        <View style={styles.sideFields}>
-          <View style={styles.countryCode}>
-            <Text>+250</Text>
+    <PaperProvider>
+      <View style={styles.background}>
+        <Image source={require("../assets/logo.png")} />
+        <View style={styles.registerForm}>
+          <Text style={errorMessageStyles}>{errorMessage.message}</Text>
+          <Text style={styles.title}>Register</Text>
+          <View style={styles.sideFields}>
+            <View style={styles.countryCode}>
+              <Text>+250</Text>
+            </View>
+            <View style={{ alignSelf: "flex-end" }}>
+              <TextInput
+                // label={ focus ? "" : "Phone"}
+                placeholder="Phone"
+                value={userData.phone}
+                onChangeText={(phone) => handlePhone(phone)}
+                style={{ width: 200, height: 40, fontSize: 10 }}
+              />
+            </View>
           </View>
-          <View style={{ alignSelf: "flex-end" }}>
+          <View style={styles.fields}>
             <TextInput
-              // label={ focus ? "" : "Phone"}
-              placeholder="Phone"
-              value={userData.phone}
-              onChangeText={(phone) => handlePhone(phone)}
-              style={{ width: 200, height: 40, fontSize: 10 }}
+              mode="flat"
+              placeholder="First name"
+              value={userData.fname}
+              onChangeText={(fname) => setUserData({ ...userData, fname })}
+              style={{ width: "100%", height: 40, fontSize: 10 }}
             />
           </View>
-        </View>
-        <View style={styles.fields}>
-          <TextInput
-            placeholder="First name"
-            value={userData.fname}
-            onChangeText={(fname) => setUserData({ ...userData, fname })}
-            style={{ width: "100%", height: 40, fontSize: 10 }}
-          />
-        </View>
-        <View style={styles.fields}>
-          <TextInput
-            placeholder="Last name"
-            onChangeText={(lname) => setUserData({ ...userData, lname })}
-            style={{ width: "100%", height: 40, fontSize: 10 }}
-          />
-        </View>
-        <View style={styles.fields}>
-          <TextInput
-            placeholder="Email"
-            value={userData.email}
-            onChangeText={(email) => setUserData({ ...userData, email })}
-            style={{ width: "100%", height: 40, fontSize: 10 }}
-          />
-        </View>
-        <View style={styles.fields}>
-          <TextInput
-            placeholder="Password"
-            onChangeText={(password) => setUserData({ ...userData, password })}
-            style={{ width: "100%", height: 40, fontSize: 10 }}
-          />
-        </View>
-        <View style={styles.fields}>
-          <TextInput
-            placeholder="Confirm Password"
-            onChangeText={(password_confirmation) =>
-              setUserData({ ...userData, password_confirmation })
-            }
-            style={{ width: "100%", height: 40, fontSize: 10 }}
-          />
-        </View>
-        <View style={styles.fields}>
-          <Text
-            style={{
-              backgroundColor: "#E5E5E5",
-              color: "#696666",
-              padding: 12,
-              fontSize: 10,
-              borderBottomWidth: 1,
-              borderBottomColor: "#696666",
-            }}
-            onPress={() => setVisible(true)}
-          >
-            I am a...
-          </Text>
-          <Portal>
-            <Dialog visible={visible} onDismiss={hideDialog}>
-              <Dialog.Title>Choose an option</Dialog.Title>
-              <Dialog.Content>
-                <View>
-                  <Text>Seller</Text>
-                  <RadioButton
-                    value="seller"
-                    status={checked === "seller" ? "checked" : "unchecked"}
-                    onPress={() => {
-                      setChecked("seller");
-                      setUserData({ ...userData, role: "seller" });
-                      setVisible(false);
-                    }}
-                  />
-                </View>
-                <View>
-                  <Text>Guest</Text>
-                  <RadioButton
-                    value="guest"
-                    status={checked === "guest" ? "checked" : "unchecked"}
-                    onPress={(value) => {
-                      setChecked("guest");
-                      setUserData({ ...userData, role: "guest" });
-                      setVisible(false);
-                    }}
-                  />
-                </View>
-              </Dialog.Content>
-            </Dialog>
-          </Portal>
-        </View>
-        <View style={[styles.fields, styles.buttons]}>
-          <Button
-            mode="contained"
-            onPress={handleSubmit}
-            color="#FF0F00"
-            style={{ borderRadius: 50, width: "80%" }}
-          >
-            Create account
-          </Button>
+          <View style={styles.fields}>
+            <TextInput
+              placeholder="Last name"
+              onChangeText={(lname) => setUserData({ ...userData, lname })}
+              style={{ width: "100%", height: 40, fontSize: 10 }}
+            />
+          </View>
+          <View style={styles.fields}>
+            <TextInput
+              placeholder="Email"
+              value={userData.email}
+              onChangeText={(email) => setUserData({ ...userData, email })}
+              style={{ width: "100%", height: 40, fontSize: 10 }}
+            />
+          </View>
+          <View style={styles.fields}>
+            <TextInput
+              secureTextEntry={true}
+              placeholder="Password"
+              onChangeText={(password) =>
+                setUserData({ ...userData, password })
+              }
+              style={{ width: "100%", height: 40, fontSize: 10 }}
+            />
+          </View>
+          <View style={styles.fields}>
+            <TextInput
+              secureTextEntry={true}
+              placeholder="Confirm Password"
+              onChangeText={(password_confirmation) =>
+                setUserData({ ...userData, password_confirmation })
+              }
+              style={{ width: "100%", height: 40, fontSize: 10 }}
+            />
+          </View>
+          <View style={styles.fields}>
+            <Text
+              style={{
+                backgroundColor: "#E5E5E5",
+                color: "#696666",
+                padding: 12,
+                fontSize: 10,
+                borderBottomWidth: 1,
+                borderBottomColor: "#696666",
+              }}
+              onPress={showDialog}
+            >
+              I am a {userData.role ? userData.role : "..."}
+            </Text>
+            <Portal>
+              <Dialog visible={visible} onDismiss={hideDialog}>
+                <Dialog.Title>Choose an option</Dialog.Title>
+                <Dialog.Content>
+                  <View>
+                    <Text>Seller</Text>
+                    <RadioButton
+                      value="seller"
+                      status={checked === "seller" ? "checked" : "unchecked"}
+                      onPress={() => {
+                        setChecked("seller");
+                        setUserData({ ...userData, role: "seller" });
+                        hideDialog();
+                      }}
+                    />
+                  </View>
+                  <View>
+                    <Text>Guest</Text>
+                    <RadioButton
+                      value="guest"
+                      status={checked === "guest" ? "checked" : "unchecked"}
+                      onPress={(value) => {
+                        setChecked("guest");
+                        setUserData({ ...userData, role: "guest" });
+                        hideDialog();
+                      }}
+                    />
+                  </View>
+                </Dialog.Content>
+              </Dialog>
+            </Portal>
+          </View>
+          <View style={[styles.fields, styles.buttons]}>
+            <Button
+              mode="contained"
+              onPress={handleSubmit}
+              color="#FF0F00"
+              style={{ borderRadius: 50, width: "80%" }}
+              disabled={
+                userData.phone &&
+                userData.fname &&
+                userData.lname &&
+                userData.email &&
+                userData.password &&
+                userData.password_confirmation &&
+                userData.role
+                  ? false
+                  : true
+              }
+            >
+              Create account
+            </Button>
+          </View>
         </View>
       </View>
-    </View>
+    </PaperProvider>
   );
 }
 const styles = StyleSheet.create({
@@ -159,11 +215,12 @@ const styles = StyleSheet.create({
     width: 411,
     height: "100%",
     alignItems: "center",
-    paddingTop: 89,
+    paddingTop: 59,
   },
   buttons: {
     alignItems: "center",
   },
+
   fields: {
     width: "85%",
     marginTop: 17,
@@ -171,7 +228,7 @@ const styles = StyleSheet.create({
     marginRight: "auto",
   },
   registerForm: {
-    width: 352,
+    width: "80%",
     height: 530,
     backgroundColor: "white",
     shadowColor: "#000",
@@ -184,7 +241,7 @@ const styles = StyleSheet.create({
     elevation: 3,
     borderRadius: 10,
     position: "absolute",
-    top: 150,
+    top: 120,
     left: 22,
   },
   title: {
