b2152b9219f791acc1f1668b8862aba8de8456d2
ClarisseU <clarisseum@gmail.com>
2020-11-16 17:06:11 +0200

    Design the drinks page

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Redirect from "./app/screens/Redirect";
 import HomePageSeller from "./app/screens/HomePageSeller";
+import TechnologyPage from "./app/screens/TechnologyPage";
+import DrinksPage from "./app/screens/DrinksPage";
 
 const Stack = createStackNavigator();
 
@@ -19,15 +21,25 @@ export default function App() {
           {/* <View style={styles.container}>
             <RegistrationScreen />
           </View> */}
-          <Stack.Screen
+          {/* <Stack.Screen
             name="Register"
             component={RegistrationScreen}
+            options={{ headerShown: false }}
+          /> */}
+           {/* <Stack.Screen
+            name="TechnologyPage"
+            component={TechnologyPage}
+            options={{ headerShown: false }}
+          /> */}
+          <Stack.Screen
+            name="DrinksPage"
+            component={DrinksPage}
             options={{ headerShown: false }}
           />
           <Stack.Screen
             name="Redirect"
             component={Redirect}
-            options={{ headerShown: false }}
+            options={{ headerShown: false }} 
           />
         </Stack.Navigator>
       </NavigationContainer>
